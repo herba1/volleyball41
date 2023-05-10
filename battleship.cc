f48c81d4b015e9c778917eb4bfa8303b6e4c4318
Rene Hernandez <hrene2412@gmail.com>
2023-05-10 00:39:18 -0700

    Fixed print bug

--- battleship.cc
+++ battleship.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include"/public/colors.h"
+#include "/public/colors.h"
 using namespace std;
 
 const int BOARD = 10;
@@ -14,13 +14,13 @@ int hits = 0;
 int misses = 0;
 
 void player1_board() {
-	cout << "  ";
+	//cout << "  ";
 	for (int col = 0; col < BOARD; col++) {
-		cout << CYAN << COL[col] << WHITE << " "; //prints the outer int colums
+	//	cout << CYAN << COL[col] << WHITE << " "; //prints the outer int colums
 	}
-	cout << endl;
+	//cout << endl;
 	for (int row = 0; row < BOARD; row++) {
-		cout << MAGENTA <<  ROW[row] << WHITE << " "; // prints the outer char rows
+	//	cout << MAGENTA <<  ROW[row] << WHITE << " "; // prints the outer char rows
 		for (int col = 0; col < BOARD; col++) {
 			board[0][0] = '*';
 			board[0][1] = '*';
@@ -34,20 +34,22 @@ void player1_board() {
 			board[7][8] = '*';		
 			board[8][8] = '*';
 			board[9][8] = '*';
-			if (board[row][col] == '*') { cout << "* "; }
-			else { cout << "- "; }
+		 //if (board[row][col] == '*') { cout << "* "; }
+		//else { cout << "- "; }
+	//	}
+	//	cout << endl;        
 		}
-		cout << endl;        
 	}
 }
+void player2_board() {
 
+}
 
 void updateBoard(int row, int col) {
   cout << "  ";
-    for (int col = 0; col < BOARD; col++) {
+  for (int col = 0; col < BOARD; col++) {
         cout << CYAN << COL[col] << WHITE << " "; //prints the outer int colums
     	}
-	// print the updated user board
     cout << endl;
     for (int i = 0; i < BOARD; i++) {
         cout << MAGENTA << ROW[i] << WHITE << " ";
@@ -55,46 +57,62 @@ void updateBoard(int row, int col) {
 			if (i == row and j == col) {  
               if (board[i][j] == '*') {
                   hit_board[i][j] = '*';  
-				 // hits ++;
+				  hits ++;
              	}
                if (board[i][j] != '*') {
 				  hit_board[i][j] = 'X';
-				   //misses ++;
+				   misses ++;
                 }
 			 }
 			   if (hit_board[i][j] == '*') {
-                cout << GREEN << "* " << WHITE;   
-	      	 }
-			   if (hit_board[i][j] == 'X') {
-				   cout << RED << "X " << WHITE;
-			   }
-			   else {
-                cout << "- ";    
-            }
+               cout << GREEN << "* " << WHITE;   
+	      	 } else if (hit_board[i][j] == 'X') {
+			   cout << RED << "X " << WHITE;
+			 } else  { cout << "- "; }  
         }
         cout << endl;
     }
+	cout << "Hits: " << hits << endl;
+	cout << "Misses: " << misses << endl;
 }
 
-		
+void updateBoard2(int row, int col) {
+
+}
+
 void battle() {
 player1_board();
-     while (hits < 12) {
+     while (true) {
+		if (hits == 12) { 
+		cout << "ALL SHIPS ARE DESTROYED, ABANDONED SHIP" << endl;
+		cin.ignore();
+		cout << "PLAYER 2 WINS" << endl;
+		exit(1);
+		}
+		if (misses == 10) {
+		cout << "YOU HAVE USED UP ALL YOUR CHANCES!" << endl;
+		exit(1);
+		}
 		char choice1;
-		int choice2  = 0;
 		cout << "Enter a row to hit a ship" << endl;
 		cin >> choice1;
+		//if (choice1.length() != 1 or (!isalpha(choice1[0]) or (toupper(choice1[0]) < 'A' or toupper(choice1[0]) > 'J'))) {
+		//	cout << "ERROR, MUST USE VALID CHAR" << endl;
+		//	continue;
+	//	}
+		int choice2;
 		cout << "Enter a col to hit a ship" << endl;
 		cin >> choice2;
-		int row = choice1 - 'A';  // convert row letter to index
+		int row = choice1 - 'A'; 
         int col = choice2 - 1;    // convert column number to index
         cout << "Your coordinates are: " << choice1 << choice2 << endl;
        	cin.ignore();
 		updateBoard(row, col);
-	 //	if (board[row][col] == 'X') { 
-	//		cout << "Hits: " << hits << endl;
-	//	 }
+		 }
 	}
+
+void battle2() {
+
 }
 
 int main() {
